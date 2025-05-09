d2f6a10b7bdd429a999a3eae07b8e4b216439ff3
wind831016 <fish88mm@gmail.com>
2025-05-09 18:43:18 +0800

    Create new Layout

---
+++ Controllers/BackstageHome/BackstageHomeController.cs
@@ -0,0 +1,19 @@
+﻿using AppDownloadListWeb.Models.View;
+using AppDownloadListWeb.Models.View.Home;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDownloadListWeb.Controllers
+{
+    public class BackstageHomeController : BaseController
+    {
+        public ActionResult Index() { 
+            return View();
+        }
+    }
+}
