5ea6e35b0f8c96703e16065b6524f55b4aaea1e8
wind831016 <fish88mm@gmail.com>
2025-05-09 11:44:53 +0800

    Create Login Page

---
+++ Controllers/Login/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using AppDownloadListWeb.Models.View;
+using AppDownloadListWeb.Models.View.Home;
+using System.Web.Mvc;
+
+namespace AppDownloadListWeb.Controllers
+{
+    public class LoginController : BaseController
+    {
+        public ActionResult Index() { 
+            return View();
+        }
+    }
+}
