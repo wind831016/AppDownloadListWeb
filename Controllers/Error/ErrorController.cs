d7b7dd8b5631aa3fdcfb8d7d2ba459f7e1469bbe
wind831016 <fish88mm@gmail.com>
2025-05-16 18:39:18 +0800

    Create error 404 page

---
+++ Controllers/Error/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AppDownloadListWeb.Controllers.Error
+{
+    public class ErrorController : BaseController
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound"); 
+        }
+
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View("ServerError"); 
+        }
+    }
+}
