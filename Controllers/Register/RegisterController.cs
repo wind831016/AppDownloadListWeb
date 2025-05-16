baf3cca3becd2c19c8d9a0b6241d9323894a61a5
wind831016 <fish88mm@gmail.com>
2025-05-16 13:55:46 +0800

    Create Register page

---
+++ Controllers/Register/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using AppDownloadListWeb.Models.View;
+using AppDownloadListWeb.Models.View.Home;
+using System.Web.Mvc;
+
+namespace AppDownloadListWeb.Controllers
+{
+    public class RegisterController : BaseController
+    {
+        public ActionResult Index() { 
+            return View();
+        }
+    }
+}
